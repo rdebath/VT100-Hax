e067cbac7434118de06929a9572b194d832af257
Claude Sylvain <csylvain@electro-technica.com>
2011-04-17 15:46:14 +0000

    Autotools configuration files update.
    CVS-ID: src/Makefile 1.2
    CVS-ID: src/Makefile.am 1.2
    CVS-ID: src/Makefile.in 1.2
    CVS-ID: Makefile 1.6
    CVS-ID: Makefile.am 1.3
    CVS-ID: Makefile.in 1.3
    CVS-ID: aclocal.m4 1.2
    CVS-ID: autogen.sh 1.2
    CVS-ID: config.log 1.2
    CVS-ID: config.status 1.2
    CVS-ID: configure 1.3
    CVS-ID: configure.ac 1.3
    CVS-ID: libtool 1.2

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
+# *****************************************************************************
+# Filename:			autogen.sh
+# Description:
+# Copyright(c):
+# Author(s):		Jay Cotton, Claude Sylvain
+# Created:			26 Mars 2011
+# Last modified:	17 April 2011
+# *****************************************************************************
+
 
 echo "Generating configure files... may take a while."
 
-autoreconf --install --force && \
-  echo "Preparing was successful if there was no error messages above." && \
-  echo "Now type:" && \
-  echo "  ./configure && make"  && \
-  echo "Run './configure --help' for more information"
+
+aclocal
+libtoolize --automake
+automake -a
+autoconf
+
 
